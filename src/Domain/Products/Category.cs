70c13d5821605cfc350ec2f81180e7ce813262fb
Pedro Lyra <pedrolyradev@outlook.com>
2022-01-28 18:08:25 -0300

    created firsts domains(Products and Categories) and firsts endpoints(Categories)

---
+++ src/Domain/Products/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace iWantApp.Domain.Products;
+
+public class Category : Entity
+{
+    public string Name { get; set; }
+    public bool Active { get; set; }
+}
---
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using iWantApp.Domain.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace iWantApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+        builder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(255);
+
+        builder.Entity<Category>()
+            .Property(c => c.Name).IsRequired();
+
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configuration)
+    {
+        configuration.Properties<string>()
+            .HaveMaxLength(100); //toda propriedade de tipo string quero q tenha no max 100 caracteres
+    }
+}
